fc564de1ff73b352f3befb213db8925791660a74
purvaa462-coder <purvaa462@gmail.com>
2026-04-07 20:04:42 +0530

    Private S3 with CloudFront OAI

--- terraform/main.tf
+++ terraform/main.tf
@@ -7,60 +7,62 @@ terraform {
   }
 
   backend "s3" {
-    bucket = "purva-terraform-state-bucket-123"   # create manually once
+    bucket = "purva-terraform-state-bucket-123"
     key    = "portfolio/terraform.tfstate"
     region = "ap-south-1"
   }
 }
+
+# Provider
 provider "aws" {
-    region = "ap-south-1"
+  region = "ap-south-1"
 }
-//for the sake of s3 uniqueness
+
+# Unique suffix
 resource "random_id" "suffix" {
   byte_length = 4
 }
-//added s3 bucket
+
+# S3 Bucket
 resource "aws_s3_bucket" "portfolio" {
   bucket = "purva-portfolio-${random_id.suffix.hex}"
 }
 
-//enabling static web hosting
-resource "aws_s3_bucket_website_configuration" "portfolio" {
-  bucket = aws_s3_bucket.portfolio.id
-
-  index_document {
-    suffix = "index.html"
-  }
-}
-
-//making bucket public
+# BLOCK PUBLIC ACCESS (UPDATED)
 resource "aws_s3_bucket_public_access_block" "portfolio" {
   bucket = aws_s3_bucket.portfolio.id
 
-  block_public_acls = false
-  block_public_policy = false
-  ignore_public_acls = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
-// adding bucket policy
+# CloudFront OAI (NEW)
+resource "aws_cloudfront_origin_access_identity" "oai" {
+  comment = "OAI for portfolio"
+}
+
+# Bucket Policy (ONLY CloudFront can access)
 resource "aws_s3_bucket_policy" "portfolio" {
   bucket = aws_s3_bucket.portfolio.id
+
   policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
       {
         Effect = "Allow"
-        Principal = "*"
-        Action = ["s3:GetObject"]
-        Resource = [
-        "${aws_s3_bucket.portfolio.arn}/*"
-        ]
+        Principal = {
+          AWS = aws_cloudfront_origin_access_identity.oai.iam_arn
+        }
+        Action   = "s3:GetObject"
+        Resource = "${aws_s3_bucket.portfolio.arn}/*"
       }
     ]
   })
 }
 
+# Upload all files
 resource "aws_s3_object" "portfolio_files" {
   for_each = fileset("../frontend/dist", "**/*")
 
@@ -83,14 +85,19 @@ resource "aws_s3_object" "portfolio_files" {
     "application/octet-stream"
   )
 }
+
+# CloudFront Distribution (UPDATED with OAI)
 resource "aws_cloudfront_distribution" "portfolio_cdn" {
   origin {
     domain_name = aws_s3_bucket.portfolio.bucket_regional_domain_name
     origin_id   = "s3-origin"
+
+    s3_origin_config {
+      origin_access_identity = aws_cloudfront_origin_access_identity.oai.cloudfront_access_identity_path
+    }
   }
 
-  enabled = true
-
+  enabled             = true
   default_root_object = "index.html"
 
   default_cache_behavior {
